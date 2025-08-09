490cf73a4b10940352fbd39ce65c112a3213c093
saurabh18kulkarni <135810129+saurabh18kulkarni@users.noreply.github.com>
2025-08-09 14:00:43 +0530

    Execute shell script 1

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo-apt install cowsay -f 
+sudo apt-get install cowsay -f 
 cowsay -f dragon " Run for the cover, I am a dragon...RAWR" >> dragon.txt
 grep -i "dragon" drgaon.txt
 cat dragon.txt 
