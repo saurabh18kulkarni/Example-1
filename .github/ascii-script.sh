f90f08e932e74d334f814b803c3abc72da0aea12
saurabh18kulkarni <135810129+saurabh18kulkarni@users.noreply.github.com>
2025-08-09 14:27:09 +0530

    change in apt-get

--- .github/ascii-script.sh
+++ .github/ascii-script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install cowsay -f 
+sudo apt-get install cowsay -y
 cowsay -f dragon " Run for the cover, I am a dragon...RAWR" >> dragon.txt
 grep -i "dragon" drgaon.txt
 cat dragon.txt 
